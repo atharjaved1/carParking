79f04c93f03fb7a730817e94fe26ac531ff7c392
AtharJaved1 <atharjavedsb@gmail.com>
2019-08-28 12:03:34 +0500

    comparepassword

--- models/user_model.js
+++ models/user_model.js
@@ -1,7 +1,6 @@
-var mongoose = require('mongoose')
-
-// bcrypt = require("bcrypt-nodejs");
-// SALT_WORK_FACTOR = 10;
+var mongoose = require('mongoose');
+bcrypt = require("bcrypt-nodejs");
+SALT_WORK_FACTOR = 10;
 
 const carparkingUserModelschema = mongoose.Schema({
     name:{
@@ -23,65 +22,41 @@ const carparkingUserModelschema = mongoose.Schema({
     }
 })
 
-
-module.exports = mongoose.model('carparkingUserModelschema',carparkingUserModelschema)
-
-
-// var mongoose = require('mongoose')
  
-// const carparkingUserModelschema = mongoose.Schema({
-
-    // name:{
-    //     type:String,
-    //     require:true
-    // },
-    // email: {   
-    //     type: String,
-    //     require: true,
-    //     index:{unique:true}
-    // },
-    // password:{
-    //     type:String,
-    //     require:true
-    // },
-    // enterdate:{
-    //     type:Date,
-    //     default:Date.now
-    // }
-// })
-
 //  pre save
 
-// carparkingUserModelschema.pre("save", function(next){
-//     // call all schema elements
-//     var carParkUserData = this;
+carparkingUserModelschema.pre("save", function(next){
+    // call all schema properties 
+    var carParkUserData = this;
 
-//     if(!carParkUserData.isModified('password')) return next();
-//     //generate salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-//         if(err)
-//         return next(err);
-//         // hash the new password using our salt
-//         bcrypt.hash(carParkUserData.password, salt, null, function(err, hash){
-//             if(err)
-//             return next(err);
-//             //set the hashed password on our user document
-//             carParkUserData.password = hash;
-//             next();
-//         });
-//     });
-// });
+    if(!carParkUserData.isModified('password')) return next();
+    //generate salt here
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+        if(err)
+        return next(err);
+        // hash the new password using our salt
+        bcrypt.hash(carParkUserData.password, salt, null, function(err, hash){
+            if(err)
+            return next(err);
+            //set the hashed password on our user document
+            carParkUserData.password = hash;
+            next();
+        });
+    });
+});
 
-// carparkingUserModelschema.method.comparePassword = function(candidatePassword, cb){
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
-//         if(err) return cb(err);
+//compare password 
+carparkingUserModelschema.method.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
 
-//         cb(null, isMatch);
-//     })
-// }
+        cb(null, isMatch);
+    })
+}
 
+//export schema
+module.exports = mongoose.model('carparkingUserModelschema',carparkingUserModelschema)
 
-// module.exports = mongoose.model('carparkingUserModelschema',carparkingUserModelschema);.
 
 
 
